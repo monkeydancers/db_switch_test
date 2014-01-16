90fb509f85c892fadde8c427c3bb9195c18725d0
Henrik Bäärnhielm <henrik@proactivehealth.se>
2014-01-16 12:00:56 +0100

    Initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DbSwitchTest::Application.config.secret_token = 'cec29667b55d3a8bcf3a53301650afa084f257770dce4e732a8999ab447007a04445c7ed62650f380d8653af98f3041ed4ccd44042c24951711dd29d553bf1a2'
